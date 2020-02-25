a4c0ad99141d40eb99411a4136cbf1e4b017eac4
MemoVDG <memovdg@gmail.com>
2020-02-24 18:23:52 -0600

    Minor changes

---
+++ healthy_life/lib/diet_page/diet_overview.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class DietOverview extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _DietOverview();
+  }
+
+}
+
+class _DietOverview extends State<DietOverview>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      child: Column(
+        children: <Widget>[
+
+        ],
+      ),
+    );
+  }
+
+}
